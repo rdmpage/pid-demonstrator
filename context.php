d2095a6c2d0488eeeb4372066952c6ed3eafacc7
Roderic Page <rdmpage@gmail.com>
2020-06-30 12:49:40 +0100

    JSON-LD context

--- context.php
+++ context.php
@@ -1,74 +1,122 @@
 <?php
 
-// Context for Annotations
+if (0)
+{
+	// https://www.w3.org/ns/anno.jsonld
+	$json = file_get_contents(dirname(__FILE__) . '/anno.jsonld');
+	$context = json_decode($json);
+}
+else
+{
 
-$context = new stdclass;
-$context->oa	= "http://www.w3.org/ns/oa#";
-$context->rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+	// Context for Annotations
 
-// so things are easy to work with in clients we rewrite @id and @type
+	$context = new stdclass;
+	$context->oa	  	= "http://www.w3.org/ns/oa#";
+	$context->dcterms 	= "http://purl.org/dc/terms/";
+	$context->rdf     	= "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+	$context->xsd		= "http://www.w3.org/2001/XMLSchema#";
 
-$context->id = '@id';
-$context->type = '@type';
+	// so things are easy to work with in clients we rewrite @id and @type
 
-$context->Annotation = new stdclass;
-$context->Annotation->{'@type'} = '@id';
-$context->Annotation->{'@id'} = 'oa:Annotation';		
+	$context->id = '@id';
+	$context->type = '@type';
 
-$context->body = new stdclass;
-$context->body->{'@type'} = '@id';
-$context->body->{'@id'} = 'oa:hasBody';	
+	$context->Annotation = new stdclass;
+	$context->Annotation->{'@type'} = '@id';
+	$context->Annotation->{'@id'} = 'oa:Annotation';		
 
-// targets and sources	
+	$context->body = new stdclass;
+	$context->body->{'@type'} = '@id';
+	$context->body->{'@id'} = 'oa:hasBody';	
 
-$context->target = new stdclass;
-$context->target->{'@type'} = '@id';
-$context->target->{'@id'} = 'oa:hasTarget';
+	// targets and sources	
 
-$context->source = new stdclass;
-$context->source->{'@type'} = '@id';
-$context->source->{'@id'} = 'oa:hasSource';
+	$context->target = new stdclass;
+	$context->target->{'@type'} = '@id';
+	$context->target->{'@id'} = 'oa:hasTarget';
 
-// selectors
+	$context->source = new stdclass;
+	$context->source->{'@type'} = '@id';
+	$context->source->{'@id'} = 'oa:hasSource';
 
-$context->selector = new stdclass;
-$context->selector->{'@type'} = '@id';
-$context->selector->{'@id'} = 'oa:hasSelector';
-
-$context->startSelector = new stdclass;
-$context->startSelector->{'@type'} = '@id';
-$context->startSelector->{'@id'} = 'oa:hasStartSelector';
-
-$context->endSelector = new stdclass;
-$context->endSelector->{'@type'} = '@id';
-$context->endSelector->{'@id'} = 'oa:hasEndSelector';
-
-$context->RangeSelector = new stdclass;
-$context->RangeSelector->{'@type'} = '@id';
-$context->RangeSelector->{'@id'} = 'oa:RangeSelector';
-
-$context->TextQuoteSelector = new stdclass;
-$context->TextQuoteSelector->{'@type'} = '@id';
-$context->TextQuoteSelector->{'@id'} = 'oa:TextQuoteSelector';
-
-$context->TextPositionSelector = new stdclass;
-$context->TextPositionSelector->{'@type'} = '@id';
-$context->TextPositionSelector->{'@id'} = 'oa:TextPositionSelector';
-
-$context->XPathSelector = new stdclass;
-$context->XPathSelector->{'@type'} = '@id';
-$context->XPathSelector->{'@id'} = 'oa:XPathSelector';
+	$context->canonical = new stdclass;
+	$context->canonical->{'@type'} = '@id';
+	$context->canonical->{'@id'} = 'oa:canonical';
 
 
-// simple terms 
+	// selectors
 
-$context->exact  = 'oa:exact';
-$context->prefix = 'oa:prefix';
-$context->suffix = 'oa:suffix';
+	$context->selector = new stdclass;
+	$context->selector->{'@type'} = '@id';
+	$context->selector->{'@id'} = 'oa:hasSelector';
 
-$context->start  = 'oa:start';
-$context->end    = 'oa:end';
+	$context->startSelector = new stdclass;
+	$context->startSelector->{'@type'} = '@id';
+	$context->startSelector->{'@id'} = 'oa:hasStartSelector';
 
-$context->value  = 'rdf:value';
+	$context->endSelector = new stdclass;
+	$context->endSelector->{'@type'} = '@id';
+	$context->endSelector->{'@id'} = 'oa:hasEndSelector';
+
+	$context->RangeSelector = new stdclass;
+	$context->RangeSelector->{'@type'} = '@id';
+	$context->RangeSelector->{'@id'} = 'oa:RangeSelector';
+
+	$context->TextQuoteSelector = new stdclass;
+	$context->TextQuoteSelector->{'@type'} = '@id';
+	$context->TextQuoteSelector->{'@id'} = 'oa:TextQuoteSelector';
+
+	$context->TextPositionSelector = new stdclass;
+	$context->TextPositionSelector->{'@type'} = '@id';
+	$context->TextPositionSelector->{'@id'} = 'oa:TextPositionSelector';
+
+	$context->XPathSelector = new stdclass;
+	$context->XPathSelector->{'@type'} = '@id';
+	$context->XPathSelector->{'@id'} = 'oa:XPathSelector';
+
+	// Note use of "@vocab" so that URI terms such as oa:identifying come out correctly
+	$context->motivation = new stdclass;
+	$context->motivation->{'@type'} = '@vocab';
+	$context->motivation->{'@id'} = 'oa:motivatedBy';
+
+	
+	$context->identifying = new stdclass;
+	$context->identifying->{'@type'} = '@id';
+	$context->identifying->{'@id'} = 'http://www.w3.org/ns/oa#identifying';
+
+
+	// simple terms 
+
+	$context->exact  = 'oa:exact';
+	$context->prefix = 'oa:prefix';
+	$context->suffix = 'oa:suffix';
+
+	$context->start  = 'oa:start';
+	$context->end    = 'oa:end';
+
+
+	$context->TextualBody    = 'oa:TextualBody';
+
+	$context->value  = 'rdf:value';
+
+
+	// other vocabs
+	$context->creator = new stdclass;
+	$context->creator->{'@type'} = '@id';
+	$context->creator->{'@id'} = 'dcterms:creator';
+
+	$context->created = new stdclass;
+	$context->created->{'@type'} = 'xsd:dateTime';
+	$context->created->{'@id'} = 'dcterms:created';
+
+	$context->modified = new stdclass;
+	$context->modified->{'@type'} = 'xsd:dateTime';
+	$context->modified->{'@id'} = 'dcterms:modified';
+}
+
+    
+
+
 
 ?>
